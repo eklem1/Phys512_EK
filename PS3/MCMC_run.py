31eb5e02c91c659b1371324627252a32071d2f3a
eklem1 <emma.klemets@mail.mcgill.ca>
2020-10-08 22:43:30 -0400

    made some plots, did some bad fits

--- PS3/MCMC_run.py
+++ PS3/MCMC_run.py
@@ -2,43 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import wmap_camb_example
 
-import corner
 
 #given code
-plt.ion()
 
 #[H_0, w_bh2, w_ch2, tau, A_s, slope]
 pars=np.asarray([65,0.02,0.1,0.05,2e-9,0.96])
-pars_old=pars
+
 
 #[multipole index, measured power spectrum, error, instrument noise part, “cosmic variance” part]
 wmap=np.loadtxt('wmap_tt_spectrum_9yr_v5.txt')
 
-plt.clf();
-# plt.errorbar(wmap[:,0],wmap[:,1],wmap[:,2],fmt='*', alpha=0.1)
-plt.plot(wmap[:,0],wmap[:,1],'.') 
 
-cmb=wmap_camb_example.get_spectrum(pars)
+#results from NM
+covA = np.array([[ 2.30233318e+02,  5.30609392e-02, -2.68331862e-01,
+         2.43283786e+00,  1.67722186e-08,  1.57350347e+00],
+       [ 5.30609392e-02,  1.25600880e-05, -6.09787460e-05,
+         5.68963928e-04,  3.95424446e-12,  3.69553416e-04],
+       [-2.68331862e-01, -6.09787460e-05,  3.17098440e-04,
+        -2.82614322e-03, -1.93734416e-11, -1.81879931e-03],
+       [ 2.43283786e+00,  5.68963928e-04, -2.82614322e-03,
+         2.66172931e-02,  1.84716026e-10,  1.69685849e-02],
+       [ 1.67722186e-08,  3.95424446e-12, -1.93734416e-11,
+         1.84716026e-10,  1.28611142e-18,  1.17828370e-10],
+       [ 1.57350347e+00,  3.69553416e-04, -1.81879931e-03,
+         1.69685849e-02,  1.17828370e-10,  1.09796327e-02]])
 
-plt.plot(cmb) #what really is the x values here then?
-plt.xlabel("multipole index")
-plt.ylabel("power spectrum")
+startVal = np.array([8.91853726e+01, 2.66745567e-02, 8.80833909e-02, 3.85185078e-01, 3.80581496e-09 ,1.10901041e+00])
 
-plt.show()
+#scaling step size based off of sigma to increase acceptance rate
+startSig = 0.5*np.array([2.67988840e+00, 6.36602154e-04, 3.77729627e-03, 1.49724000e-02, 1.10882123e-10, 1.41553442e-02])
 
-# curvature matrix from only 5 params, need to add in tau
-covA = np.array([[ 2.44781662e+02,  5.63475243e-02, -2.87916355e-01,
-         2.61416820e+00,  1.78154791e-08,  1.67922071e+00],
-       [ 5.63475243e-02,  1.32998969e-05, -6.54143274e-05,
-         6.10101376e-04,  4.18922768e-12,  3.93431234e-04],
-       [-2.87916355e-01, -6.54143274e-05,  3.43057631e-04,
-        -3.06548839e-03, -2.07813959e-11, -1.96005473e-03],
-       [ 2.61416820e+00,  6.10101376e-04, -3.06548839e-03,
-         2.88468680e-02,  1.97811728e-10,  1.82789826e-02],
-       [ 1.78154791e-08,  4.18922768e-12, -2.07813959e-11,
-         1.97811728e-10,  1.36063573e-18,  1.25409751e-10],
-       [ 1.67922071e+00,  3.93431234e-04, -1.96005473e-03,
-         1.82789826e-02,  1.25409751e-10,  1.17452872e-02]])
+
 # covA = np.eye(6)
 
 def is_pos_def(x):
@@ -57,12 +51,19 @@ def our_chisq(data,pars):
     noise=data[2]
     
     #set chi to inf if tau is negative to not get these terms
-    if pars[3] < 0:
-        chisq = np.inf
-    else:
+    
+#    tau bound - 0.0544 ± 0.0073
+    if (0.0544 - 0.0073) < pars[3] < (0.0544 + 0.0073):
         model=cmbfun(x,pars)[2:]
-
         chisq=np.sum( (y-model)**2/noise**2)
+#        print("do it")
+        
+#    if 0 <= pars[3]:
+#        model=cmbfun(x,pars)[2:]
+#        chisq=np.sum( (y-model)**2/noise**2)
+    else:
+        chisq = np.inf
+
     return chisq
 
 
@@ -74,13 +75,18 @@ def run_mcmc(pars, data, par_step, chifun, nstep=5000):
     
     chi_cur=chifun(data,pars)
     L = np.linalg.cholesky(covA);   
-    d = np.dot(L, np.random.randn(L.shape[0])) 
+#    d = np.dot(L, np.random.randn(L.shape[0])) 
+#    print(d)
+    accept_rate = np.zeros(nstep)
+    naccept = 0
     
     for i in range(nstep):
         
 #        pars_trial=pars+np.random.randn(npar)*par_step 
-        pars_trial=pars+d*0.5 #for using corvar for steps
-
+#        pars_trial=pars + par_step*np.dot(L, np.random.randn(L.shape[0])) #makes h too small?
+        pars_trial=pars + 0.5*np.dot(L, np.random.randn(L.shape[0])) #for using corvar for steps
+#        print(i, pars_trial)
+        
         chi_trial=chifun(data,pars_trial)
         
         #we now have chi^2 at our current location
@@ -90,13 +96,17 @@ def run_mcmc(pars, data, par_step, chifun, nstep=5000):
         if np.random.rand(1)<accept_prob: #accept the step with appropriate probability
             pars=pars_trial
             chi_cur=chi_trial
+            naccept += 1
+            
         chain[i,:]=pars
         chivec[i]=chi_cur
+        accept_rate[i] = naccept / (i+1)
+#        print(accept_rate[i])
         
         if i%100==0:
             print("{:.2f} ".format(i/nstep), end = '')
     print("done")
-    return chain,chivec
+    return chain, chivec, accept_rate
 
 
 x=np.linspace(1,10,1801)
@@ -106,7 +116,7 @@ print(len(x), x[-1])
 #get starting points with newton's method?
 
 # par_sigs=np.asarray([0.01,0.01,0.01,0.01])
-par_sigs=np.asarray([65,0.02,0.1,0.05,2e-9,0.96])/100
+par_sigs=np.asarray([65,0.02,0.1,0.05,2e-9,0.96])/80
 
 #[6.05254431e-01 2.53491162e-04 3.22311588e-04 5.02164207e-12
 # 3.39397857e-03]
@@ -117,13 +127,22 @@ pars_guess=np.asarray([65,0.02,0.1,0.05,2e-9,0.96])
 
 data=[wmap[:,0], wmap[:,1], wmap[:,2]]
 
-chain,chivec=run_mcmc(pars_guess, data, par_sigs, our_chisq, nstep=4000)
+#chain,chivec=run_mcmc(pars_guess, data, par_sigs, our_chisq, nstep=4000)
+chain,chivec, accept_rate=run_mcmc(pars_guess, data, startSig, our_chisq, nstep=2000)
 
 #cut off burn in
 pars_sigs_new=np.std(chain,axis=0)
 pars_new=np.mean(chain,axis=0)
+pars_ave=np.mean(accept_rate)
 
-print(pars_sigs_new, pars_new)
+print(pars_ave, pars_new, pars_sigs_new)
 
-np.savetxt("MCMCrun_chains1.txt", chain)
-np.savetxt("MCMCrun_chi1.txt", chivec)
+#np.savetxt("MCMCrun_chains.txt", chain)
+#np.savetxt("MCMCrun_chi.txt", chivec)
+#np.savetxt("MCMCrun_AR.txt", accept_rate)
+
+#problem if my starting tau is quite far out of tau range)
+#runs much faster? but weird chains, seems to plateau lots
+np.savetxt("MCMCrun_chains_tau.txt", chain)
+np.savetxt("MCMCrun_chi_tau.txt", chivec)
+np.savetxt("MCMCrun_AR_tau.txt", accept_rate)
