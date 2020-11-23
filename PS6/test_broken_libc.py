fa33901b4617140123cbe5163e0f6fa8d0da86d4
eklem1 <emma.klemets@mail.mcgill.ca>
2020-11-23 13:02:26 -0500

    mostly working, not sure about scaling for 3

---
+++ PS6/test_broken_libc.py
@@ -0,0 +1,48 @@
+#python routine to help show how broken the C 
+#standard library random number generator is.
+#generate a bunch of random triples.  If plotted
+#correctly, it becomes obvious they aren't 
+#anywhere close to random.
+
+import numpy as np
+import ctypes
+import numba as nb
+import time
+from matplotlib import pyplot as plt
+
+#You may need to change the name of the library where libc.so would be standard under a Linux system, and you can google for Windows
+mylib=ctypes.cdll.LoadLibrary("libc.dylib") 
+
+rand=mylib.rand
+rand.argtypes=[]
+rand.restype=ctypes.c_int
+
+
+@nb.njit
+def get_rands_nb(vals):
+    n=len(vals)
+    for i in range(n):
+        vals[i]=rand()
+    return vals
+
+def get_rands(n):
+    vec=np.empty(n,dtype='int32')
+    get_rands_nb(vec)
+    return vec
+
+
+n=300000000
+vec=get_rands(n*3)
+#vv=vec&(2**16-1)
+
+vv=np.reshape(vec,[n,3])
+vmax=np.max(vv,axis=1)
+
+maxval=1e8
+vv2=vv[vmax<maxval,:]
+
+f=open('rand_points_mine.txt','w')
+for i in range(vv2.shape[0]):
+    myline=repr(vv2[i,0])+' '+repr(vv2[i,1])+' '+ repr(vv2[i,2])+'\n'
+    f.write(myline)
+f.close()
